6f25554cdeeda365d21204355e6530e3d10a0494
naskete <1776795871@qq.com>
2021-09-22 17:16:32 +0800

    feat:add code annotation

--- vggnet.py
+++ vggnet.py
@@ -17,17 +17,18 @@ labels = []
 
 # 初始化参数
 INIT_LR = 0.001  # 学习率
-EPOCH = 600  # 训练轮次
+EPOCH = 100  # 训练轮次
 BITCH_SIZE = 30
 
 
 def read_image_set(dataset, label, catalogue, random_state, size=64):
     """
-    :param dataset:
-    :param label:
-    :param catalogue:
-    :param random_state:
-    :param size:
+    读取训练数据集
+    :param dataset: list 存储训练数据集图片
+    :param label: list 存储每一张图片对应标签（类别）
+    :param catalogue: 数据集根目录
+    :param random_state: 随机数种子
+    :param size: 图片预处理resize大小
     :return:
     """
     print('[INFO]start reading image data...')
@@ -45,17 +46,18 @@ def read_image_set(dataset, label, catalogue, random_state, size=64):
             image = cv.resize(image, (size, size))
             dataset.append(image)
             # 获取对应标签，即文件夹的名字
-            lb = target.split(os.path.sep)[-2]
-            label.append(lb)
+            lab = target.split(os.path.sep)[-2]
+            label.append(lab)
 
 
 def build_model(width, height, depth, classes):
     """
-    :param width:
-    :param height:
-    :param depth:
-    :param classes:
-    :return:
+    构建模型
+    :param width: 图片resize的宽度
+    :param height: 图片resize的高度
+    :param depth: rgb 取depth = 3
+    :param classes: classes = len(lb.classes_)
+    :return: model 返回模型
     """
     model = Sequential()
     input_shape = (height, width, depth)
@@ -132,7 +134,7 @@ def build_model(width, height, depth, classes):
     model.add(Dense(1024))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.35))
 
     # 分类
     model.add(Dense(classes))
@@ -143,8 +145,9 @@ def build_model(width, height, depth, classes):
 
 def draw_figure(his, epoch):
     """
-    :param his:
-    :param epoch:
+    绘制结果loss accuracy 折线图
+    :param his: 训练结果
+    :param epoch: epoch值
     :return:
     """
     n = np.arange(0, epoch)
@@ -158,10 +161,21 @@ def draw_figure(his, epoch):
     plt.xlabel('epoch')
     plt.ylabel('loss/accuracy')
     plt.legend()
-    plt.savefig('image/loss-accuracy-vgg.png')
+    plt.savefig('figure/loss-accuracy-vgg.png')
 
 
 def train_model(models, train_set_x, test_set_x, train_set_y, test_set_y, epoch, bitch):
+    """
+    训练模型
+    :param models: model 要训练的模型
+    :param train_set_x: 训练集x
+    :param test_set_x: 测试集x
+    :param train_set_y: 训练集y
+    :param test_set_y: 测试集y
+    :param epoch: epoch值
+    :param bitch: bitch_size
+    :return: h 返回训练结果
+    """
     print('[INFO]开始训练模型...')
     opt = optimizers.gradient_descent_v2.SGD(lr=INIT_LR, decay=INIT_LR / epoch)
     models.compile(loss=keras.losses.categorical_crossentropy, optimizer=opt, metrics=['accuracy'])
@@ -177,6 +191,14 @@ def train_model(models, train_set_x, test_set_x, train_set_y, test_set_y, epoch,
 
 
 def save(models, label, model_name, label_name):
+    """
+    保存模型与标签
+    :param models: model 模型
+    :param label: label 标签
+    :param model_name: 保存的模型名字
+    :param label_name: 保存的标签名，后缀使用.pickle
+    :return:
+    """
     print('[INFO]保存模型...')
     models.save(model_name)
     f = open(label_name, 'wb')
